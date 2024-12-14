642769ece40809c3995055b848686f738a695a31
Ruslan05550 <228Pyclan228@gmail.com>
2024-12-14 18:16:47 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,5 @@
+homework = 12
+time = 1.5
+course = 'Python'
+time_homework = time / homework
+print ("Курс:", course + ',',"всего задач:" + str(homework) + (','), "затрачено часов:",str(time) + (','), "среднее время выполнения:", time_homework, "часа")
